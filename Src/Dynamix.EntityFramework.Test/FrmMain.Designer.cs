86684ddbc3db0672e2c10dbcb9b8cc1dc0daa491
Ardy Malihi <ardymalihi@builddirect.com>
2016-08-23 19:11:20 -0700

    Removed Connection String

--- Src/Dynamix.EntityFramework.Test/FrmMain.Designer.cs
+++ Src/Dynamix.EntityFramework.Test/FrmMain.Designer.cs
@@ -82,7 +82,7 @@
             this.txtConnectionString.Name = "txtConnectionString";
             this.txtConnectionString.Size = new System.Drawing.Size(572, 20);
             this.txtConnectionString.TabIndex = 9;
-            this.txtConnectionString.Text = "Data Source=v3dedb01;Initial Catalog=RatingsAndReviews;Integrated Security=True";
+            this.txtConnectionString.Text = "Data Source=[Server];Initial Catalog=[db];Integrated Security=True";
             // 
             // label1
             // 
